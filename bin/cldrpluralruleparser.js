eddd4e73302a469f96612c382084b4b3a6bb194b
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2018-03-28 15:59:21 +0530

    Fix the typo in the name of cldrpluralruleparser

--- bin/cldrpluralruleparser.js
+++ bin/cldrpluralruleparser.js
@@ -4,7 +4,7 @@ var parser = require(__dirname + '/../src/CLDRPluralRuleParser.js');
 if (process.argv.length < 3) {
     process.stdout.write("Please provide the rule and a number to test\n");
     process.stdout.write("Example:\n");
-    process.stdout.write("cldrpluraltuleparser 'v = 0 and n != 0..10 and n % 10 = 0' 20\n");
+    process.stdout.write("cldrpluralruleparser 'v = 0 and n != 0..10 and n % 10 = 0' 20\n");
 } else {
     var result = parser(process.argv[2], process.argv[3]);
     process.stdout.write(result);
