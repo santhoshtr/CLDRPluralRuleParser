a6e52821ff3af3578598d08f5e6dc9058538dd63
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2021-05-18 14:55:45 +0530

    Provide browser, cjs, esm builds

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,46 @@ import pkg from './package.json'
 import esbuild from 'rollup-plugin-esbuild'
 
 export default [
+  // browser-friendly UMD build
   {
     input: 'src/CLDRPluralRuleParser.js',
     output: {
       name: 'pluralRuleParser',
-      file: pkg.main,
+      file: pkg.browser,
       format: 'umd'
     },
     plugins: [
-      esbuild()
+      esbuild({
+        minify: true
+      })
+    ]
+  },
+  // CommonJS (for Node) build.
+  {
+    input: 'src/CLDRPluralRuleParser.js',
+    output: {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'default'
+    },
+    plugins: [
+      esbuild({
+        minify: true
+      })
+    ]
+  },
+  // ES module (for bundlers) build.
+  {
+    input: 'src/CLDRPluralRuleParser.js',
+    output: {
+      file: pkg.module,
+      format: 'es',
+      exports: 'default'
+    },
+    plugins: [
+      esbuild({
+        minify: true
+      })
     ]
   }
 ]
